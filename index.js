f595f76874cd53aa315035d798deda9784d3d842
GarMaCodes <75856890+GarMaCodes@users.noreply.github.com>
2021-05-18 16:41:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json({ extended: true }));
 
 //Puerto del servidor
-const PORT = process.env.PORT || 4000; //Si no existe el puerto se asigna 4000
+const port = process.env.port || 4000; //Si no existe el puerto se asigna 4000
 
 //Importando rutas Middlewares
 app.use("/api/usuarios", require("./routes/usuarios"));
@@ -24,6 +24,6 @@ app.use("/api/proyectos", require("./routes/proyectos"));
 app.use("/api/tareas", require("./routes/tareas"));
 
 //Arrancando el servidor
-app.listen(PORT, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`El servidor está funcionando en el puerto ${PORT}`);
 });
